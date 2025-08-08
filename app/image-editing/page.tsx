a1efb4ed4a53b4516b65fa533cfe9314b8591a42
chandan yadav <nippiyadav890@gmail.com>
2025-08-08 12:51:18 +0530

    new update

--- app/image-editing/page.tsx
+++ app/image-editing/page.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { Copy, Image, ImageUpIcon, InspectIcon, Layers, ShapesIcon, Trash2Icon } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
+import { BringToFront, Copy, Download, Image, ImageUpIcon, InspectIcon, Layers, SendToBack, ShapesIcon, StepBack, StepForward, Trash2Icon } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react'
+import html2canvas from 'html2canvas';
 import { DraggableEvent } from 'react-draggable';
 import { DraggableData, Position, ResizableDelta, Rnd } from "react-rnd";
 
@@ -45,6 +46,7 @@ interface StateProps {
   bgColor: string
   borderRadius?: number
   src?: string | null
+  order: number
   scale?: number
   filter?: Partial<FilterProps>
   blendMode: BlendMode
@@ -54,7 +56,7 @@ function ProImageEditor() {
   const [activeId, setActiveId] = useState<string>("");
 
   const [state, setState] = useState<StateProps[]>([
-    { id: "image_1", x: 0, y: 0, width: 300, height: 250, bgColor: "#fff", src: null, blendMode: "normal", scale: 1 }
+    { id: "image_1", x: 0, y: 0, width: 300, height: 250, bgColor: "#fff", src: null, blendMode: "normal", scale: 1, order: 0 }
   ]);
 
   const BLEND_MODES: BlendMode[] = [
@@ -76,8 +78,10 @@ function ProImageEditor() {
     "luminosity",
   ];
 
+  const randomId = Math.round(Math.random() * 1000);
+
   const shapeInserting = () => {
-    const randomId = Math.round(Math.random() * 1000)
+    const maxOrder = Math.max(...state.map((v, i) => v.order ?? 0))
     const newShape: StateProps = {
       id: `image_${randomId}`,
       x: 0,
@@ -85,6 +89,7 @@ function ProImageEditor() {
       width: 300,
       height: 250,
       bgColor: "#fff",
+      order: maxOrder + 1,
       blendMode: "normal",
       scale: 1
     }
@@ -143,15 +148,23 @@ function ProImageEditor() {
     const ImageUrl = URL.createObjectURL(file[0]);
 
     console.log("ImageUrl:- ", ImageUrl);
+    const maxOrder = state.length > 0 ? Math.max(...state.map((v, i) => v.order ?? 0)) : 0;
+
+
     const newShape: StateProps = {
-      id: `image_${file[0].name}`, x: 0, y: 0, width: 300, height: 250, bgColor: "#fff", scale: 1, src: ImageUrl, blendMode: "normal"
+      id: `image_${file[0].name}`, x: 0, y: 0, width: 300, height: 250, bgColor: "#fff", scale: 1, src: ImageUrl, blendMode: "normal", order: maxOrder + 1
     }
 
     setState((prev) => [newShape, ...prev])
   }
 
   const copyLayer = (selectedId: string) => {
+    const maxOrder = Math.max(...state.map((v, i) => v.order))
     const selectDiv: StateProps | undefined = state.find((v, i) => v.id === selectedId);
+
+    console.log("copyingDiv:- ",selectDiv);
+    
+
 
     if (!selectDiv) {
       return
@@ -159,11 +172,99 @@ function ProImageEditor() {
 
     const newDiv = {
       ...selectDiv,
-      id: selectDiv.id + "copy"
+      id: selectDiv.id + randomId + "copy",
+      order: maxOrder + 1
     };
 
     setState((prev) => [newDiv, ...prev])
 
+  }
+
+  const backFn = (selectedId: string) => {
+    // from this i am getting lowest order
+    const minOrder = Math.min(...state.map((v, i) => v.order ?? 0))
+    setState((prev) => {
+      // from this setting that lowest to the selectedId order and other are as it is 
+      const changeSelectedOrder = prev.map((l, i) => l.id === selectedId ? { ...l, order: minOrder - 1 } : l);
+      console.log("changeSelectedOrder:- ", changeSelectedOrder);
+      // this will rearrange all in small to big order number object, i am doing so because i have to increase the order for giving a new order id , because minOrder is 0 then -1 then value will be negative
+      const sortAccordingToOrder = changeSelectedOrder.sort((small, big) => small.order ?? 0 - big.order);
+      console.log("sortedOrder:- ", sortAccordingToOrder);
+      // i am giving rearrange as they are ordered
+      const newLayerUpdatedOrder = sortAccordingToOrder.map((v, i) => ({ ...v, order: i }));
+      console.log("newLayerUpdatedOrder:- ", newLayerUpdatedOrder);
+      return newLayerUpdatedOrder
+    })
+  }
+
+  const frontFn = (selectedId: string) => {
+    const maxOrder = state.length > 0 ? Math.max(...state.map((v, i) => v.order)) : 0;
+
+    setState((prev) => {
+      return prev
+        .map((l, i) => l.id === selectedId ? { ...l, order: maxOrder + 1 } : l)
+        .sort((s, b) => s.order - b.order)
+        .map((l, i) => ({ ...l, order: i }))
+    })
+
+  }
+
+  const backwardFn = (selectedId: string) => {
+    // mujhko jis order id par hai us se ek pichhe karna hai,
+    setState((prev) => {
+      return prev
+        .map((l, i) => l.id === selectedId ? { ...l, order: l.order - 1 } : { ...l, order: l.order + 1 })
+        .sort((s, b) => s.order - b.order)
+        .map((l, i) => ({ ...l, order: i }))
+    })
+  };
+  const forwardFn = (selectedId: string) => {
+    // mujhko jis order id par hai us se ek pichhe karna hai,
+    setState((prev) => {
+      return prev
+        .map((l, i) => l.id === selectedId ? { ...l, order: l.order + 1 } : { ...l, order: l.order - 1 })
+        .sort((s, b) => s.order - b.order)
+        .map((l, i) => ({ ...l, order: i }))
+    })
+  };
+
+  const removeUnsupportedColors = (element: HTMLElement) => {
+  const all = element.querySelectorAll<HTMLElement>("*");
+  all.forEach(el => {
+    const bg = getComputedStyle(el).backgroundColor;
+    if (bg.includes("oklch")) {
+      console.log("oklch El:- ",el);
+      
+      el.style.backgroundColor = "#ffffff"; // or any fallback color
+    }
+
+    const color = getComputedStyle(el).color;
+    if (color.includes("oklch")) {
+      el.style.color = "#000000";
+    }
+  });
+};
+
+  const canvasRef = useRef<HTMLDivElement | null>(null);
+  const exporting = async ()=>{
+    if (canvasRef.current) {
+      removeUnsupportedColors(canvasRef.current);
+      // Take screenshot of this DOM element
+      const canvas = await html2canvas(canvasRef.current, {
+        backgroundColor: null, // Make background transparent
+        useCORS: true,         // Allow loading external images
+        scale: 2               // Improve export quality (like retina)
+      });
+
+      // Convert the canvas to a data URL (base64 image)
+      const image = canvas.toDataURL('image/png');
+
+      // Create a temporary link and download the image
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'exported-canvas.png';
+      link.click();
+    }
   }
 
   return (
@@ -185,17 +286,24 @@ function ProImageEditor() {
                 if (e.target.files) { fileInserting(e.target.files) }
               }} />
             </label>
+            {/* export */}
+            <label className='flex gap-2 bg-white rounded-md w-fit p-2 cursor-pointer' onClick={exporting}>
+              <Download  size={22} color='black' />
+              <span>Export</span>
+              
+            </label>
             {/* layer */}
             <div>
               <span>
                 <Layers size={22} color='black' />
               </span>
               <div className='flex flex-col gap-2 mt-2'>
-                {state.length > 0 && state.map((v, i) => (
+                {/* slice is being use for shallow copy other wise sort will mutate the state  */}
+                {state.length > 0 && state.slice().sort((s, b) => b.order - s.order).map((v, i) => (
                   <div key={v.id}>
-                    <span onClick={() => setActiveId(v.id)} className={`w-full flex gap-2 justify-between cursor-pointer p-2 rounded-md group/delete hover:bg-gray-400 ${activeId === v.id ? "bg-gray-400" : ""}`}>
-                      <span>File {v.id}</span>
-                      <span className='flex'>
+                    <span onClick={() => setActiveId(v.id)} className={`w-full flex gap-2 justify-between cursor-pointer p-2 rounded-md relative group/delete overflow-hidden hover:bg-gray-400 ${activeId === v.id ? "bg-gray-400" : ""}`}>
+                      <span className='line-clamp-2'>File {v.id}</span>
+                      <span className=' absolute top-2 right-2 flex'>
                         <Copy onClick={() => copyLayer(v.id)} size={25} className='hover:bg-green-700/30 p-1 rounded-md cursor-pointer hidden group-hover/delete:block' />
                         <Trash2Icon onClick={() => deleteLayer(v.id)} size={25} className='hover:bg-amber-700/90 p-1 rounded-md cursor-pointer hidden group-hover/delete:block' />
                       </span>
@@ -210,78 +318,95 @@ function ProImageEditor() {
         <div className='bg-black flex-1 flex justify-center items-center relative' style={{ height: `calc(-84px + 100vh)` }}>
           {/* styling */}
           <div className='absolute top-0 w-full p-2'>
-              {activeId.trim().length>0 &&
-            <div className='bg-white rounded-md shadow-md p-2 flex justify-center items-center'>
+            {activeId.trim().length > 0 &&
+              <div className='bg-white rounded-md shadow-md p-2 flex justify-center items-center'>
                 {[state.find((v, i) => v.id === activeId)].map((v, i) => (
-                  <div className='flex gap-2 justify-center' key={i}>
-                    <span>
-                      <span className='font-bold'>File </span> {v?.id??""}</span>
-                    <span>
-                      <span className='font-bold'>Width </span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.width??0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { width: Number(e.target.value) }) }} />}
-                    </span>
-                    <span>
-                      <span className='font-bold'>Height </span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.height??0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { height: Number(e.target.value) }) }} />}
-                    </span>
-                    <span>
-                      <span className='font-bold'>Position-X </span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.x??0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { x: Number(e.target.value) }) }} />}
-                    </span>
-                    <span>
-                      <span className='font-bold'>Position-Y </span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.y??0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { y: Number(e.target.value) }) }} />}
-                    </span>
-                    <span>
-                      <span className='font-bold'>Blend </span>
-                      {activeId && <select
-                        className='font-bold inputNumStyle'
-                        style={{ width: "100px" }}
-                        onChange={(e) => {
-                          shapeDesignDiv(v?.id ? v.id : "", { blendMode: e.target.value as BlendMode })
-                        }}
-                        value={v?.blendMode??"normal"} name="blendMode" id="blendMode">
-                        {BLEND_MODES.map((v, i) => (
-                          <option className='font-bold' key={i} id={"blendMode"} value={v}>
-                            {v}
-                          </option>
-                        ))}
-                      </select>}
-                    </span>
-                    <span>
-                      <span className='font-bold mr-2'>Scale</span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.scale??1} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { scale: Number(e.target.value) }) }} />}
+                  <div className='flex gap-2 flex-col flex-wrap' key={i}>
+                    <div className='flex gap-2 flex-wrap'>
+                      <span>
+                        <span className='font-bold'>File </span> {v?.id ?? ""}</span>
+                      <span>
+                        <span className='font-bold'>Width </span>
+                        {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.width ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { width: Number(e.target.value) }) }} />}
+                      </span>
+                      <span>
+                        <span className='font-bold'>Height </span>
+                        {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.height ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { height: Number(e.target.value) }) }} />}
+                      </span>
+                      <span>
+                        <span className='font-bold'>Position-X </span>
+                        {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.x ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { x: Number(e.target.value) }) }} />}
+                      </span>
+                      <span>
+                        <span className='font-bold'>Position-Y </span>
+                        {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.y ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { y: Number(e.target.value) }) }} />}
+                      </span>
+                      <span>
+                        <span className='font-bold'>Blend </span>
+                        {activeId && <select
+                          className='font-bold inputNumStyle'
+                          style={{ width: "100px" }}
+                          onChange={(e) => {
+                            shapeDesignDiv(v?.id ? v.id : "", { blendMode: e.target.value as BlendMode })
+                          }}
+                          value={v?.blendMode ?? "normal"} name="blendMode" id="blendMode">
+                          {BLEND_MODES.map((v, i) => (
+                            <option className='font-bold' key={i} id={"blendMode"} value={v}>
+                              {v}
+                            </option>
+                          ))}
+                        </select>}
+                      </span>
+                      <span>
+                        <span className='font-bold mr-2'>Scale</span>
+                        {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.scale ?? 1} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { scale: Number(e.target.value) }) }} />}
 
-                    </span>
-                    <span className='spanStyle'>
-                      <span className='font-bold mr-2'>Color</span>
-                      {activeId && <input type="color" className='h-[30px]' min={1} value={v?.bgColor??"#fff"} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { bgColor: (e.target.value) }) }} />}
+                      </span>
+                      <span className='spanStyle'>
+                        <span className='font-bold mr-2'>Color</span>
+                        {activeId && <input type="color" className='h-[30px]' min={1} value={v?.bgColor ?? "#fff"} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { bgColor: (e.target.value) }) }} />}
 
-                    </span>
-                    <span className='spanStyle'>
-                      <span className='font-bold mr-2'>Border Radius</span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.borderRadius ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { borderRadius: Number(e.target.value) }) }} />}
-                    </span>
-                    <span className='spanStyle'>
-                      <span className='font-bold mr-2'>Back</span>
-                      {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.borderRadius ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { borderRadius: Number(e.target.value) }) }} />}
-                    </span>
+                      </span>
+                      <span className='spanStyle'>
+                        <span className='font-bold mr-2'>Border Radius</span>
+                        {activeId && <input type="number" className='inputNumStyle' min={1} value={v?.borderRadius ?? 0} onChange={(e) => { shapeDesignDiv(v?.id ? v.id : "", { borderRadius: Number(e.target.value) }) }} />}
+                      </span>
+                    </div>
+                    {/* front back forward backward */}
+                    <div className='flex gap-2 flex-wrap'>
+                      <span className='spanStyle'>
+                        <span className='font-bold mr-2'>Back</span>
+                        {activeId && <SendToBack size={22} onClick={() => backFn(v?.id ?? "")} />}
+                      </span>
+                      <span className='spanStyle'>
+                        <span className='font-bold mr-2'>Front</span>
+                        {activeId && <BringToFront size={22} onClick={() => frontFn(v?.id ?? "")} />}
+                      </span>
+                      <span className='spanStyle'>
+                        <span className='font-bold mr-2'>StepBack</span>
+                        {activeId && <StepBack size={22} onClick={() => backwardFn(v?.id ?? "")} />}
+                      </span>
+                      <span className='spanStyle'>
+                        <span className='font-bold mr-2'>StepForward</span>
+                        {activeId && <StepForward size={22} onClick={() => forwardFn(v?.id ?? "")} />}
+                      </span>
+                    </div>
                   </div>
                 ))}
-            </div>
-              }
+              </div>
+            }
           </div>
-
-          <div onClick={(e) => {
+            {/* layer */}
+          <div ref={canvasRef} onClick={(e) => {
             setActiveId("")
-          }} className='w-1/2 h-1/2 bg-amber-700 overflow-hidden relative' style={{ aspectRatio: "16/9" }}>
+          }} className='w-1/2 h-1/2 bg-[#000] outline-dashed outline-1 outline-amber-50 overflow-hidden relative' style={{ aspectRatio: "16/9" }}>
             {state.length > 0 && state.map((v, i) => (
               <Rnd key={v.id}
                 position={{ x: v.x, y: v.y }}
                 size={{ height: v.height, width: v.width }}
                 className={`absolute overflow-hidden ${v.id === activeId ? "border-2 border-black" : ""}`}
                 default={{ height: v.height, width: v.width, x: v.x, y: v.y }}
-                style={{ mixBlendMode: `${v.blendMode}`, borderRadius: `${v.borderRadius}%` }}
+                style={{ mixBlendMode: `${v.blendMode}`, borderRadius: `${v.borderRadius}%`, zIndex: `${v.order}` }}
                 onDragStart={(e) => {
                   setActiveId(v.id)
                 }}
