4cc334515b578376d389ded618544c77aef51246
chandan yadav <nippiyadav890@gmail.com>
2025-09-12 22:00:02 +0530

    middleware changes

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware} from "@/auth";
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(req:NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // ✅ Skip auth check for this API
+  if (pathname.startsWith("/api/mongoose")) {
+    return NextResponse.next();
+  }
+
+  return auth;
+}
