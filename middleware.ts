224ada8b1d88e3fd50fd2b72fc2938ab01fc8ceb
chandan yadav <nippiyadav890@gmail.com>
2025-09-15 17:14:14 +0530

    changes in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,39 +2,36 @@
 import { NextRequest, NextResponse } from "next/server"
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
-import {getToken} from "next-auth/jwt"
+import { getToken } from "next-auth/jwt"
 
 // this code is very important if you want to use the mongodb and google, because middleware always run on the edge if you try to run the credentials where using database using funtion which run on the heavy machine then always will throw error, so i am first only exporting providers in authConfig which run on the edge, this tricks came after 2 days
 export const { auth } = NextAuth(authConfig)
 
-export default auth(async function middleware(req: NextRequest) {
-    
+export default auth(async (req) => {
+
     const token = await getToken({
         req,
-        secret:process.env.NEXTAUTH_SECRET
-    })
+        secret: process.env.NEXTAUTH_SECRET
+    });
     console.log(
-        "middleware:- ",token, 
+        "middleware:- ", token,
         "req.nextUrl.origin:- ", req.nextUrl.origin,
-        "AUTH_SECRET:- ", process.env.NEXTAUTH_SECRET
+        "AUTH_SECRET:- ", process.env.NEXTAUTH_SECRET,
+        "authValue", req.auth?.user
     );
 
-    const {pathname} = req.nextUrl
-    // console.log("pathname:- ",pathname,"url:- ", req.url);
-    // const authvalue = await auth();
-    // console.log("authValue:- ", authvalue?.user);
-    
-    
+    const { pathname } = req.nextUrl;
+
     if (pathname === "/image-editing") {
-        if ( !token || !token?._id) {
-            return NextResponse.redirect(new URL("/login/signin",req.nextUrl.origin))
+        if (!token || !token?._id) {
+            return NextResponse.redirect(new URL("/login/signin", req.nextUrl.origin))
         }
     }
 
-    NextResponse.next()
+    NextResponse.next();
 })
 
 
 export const config = {
-    matcher:"/image-editing"
+    matcher: "/image-editing"
 }
