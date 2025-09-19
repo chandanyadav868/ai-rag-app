c6196464c2a152d8277b18538c78581c9a09b880
chandan yadav <nippiyadav890@gmail.com>
2025-09-19 14:22:27 +0530

    useing trustHost true for

--- auth.ts
+++ auth.ts
@@ -177,7 +177,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt"
   },
-  secret:process.env.NEXTAUTH_SECRET
+  secret:process.env.NEXTAUTH_SECRET,
+  trustHost: true,
 
 })
 
